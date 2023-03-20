30cfab876f43b6ad4f4f857ff597f4ac994313a1
r.oleg.official <r.oleg.official@gmail.com>
2023-03-20 11:56:26 +0300

    Add Ex001 class "Point2D" lecture01/PointMethodEx001.java.

---
+++ src/lecture01/Point2D.java
@@ -0,0 +1,17 @@
+package lecture01;
+
+import jdk.jshell.Snippet;
+
+public class Point2D {
+    int x, y;
+
+    public Point2D(int valueX, int valueY) {
+        x = valueX;
+        y = valueY;
+    }
+
+    // Snippets
+    // ctor  - don't work
+    // get; set;
+    // docs - don't work
+}
